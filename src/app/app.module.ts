83e86a7479d7c358044d417d34796f5955487a51
megacubis <ilkin_the_best@mail.ru>
2018-11-27 13:53:18 +0300

    Work with ngIf

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
